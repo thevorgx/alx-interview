d60fe30231b05a2c9024e6d8098f931598f39b07
thevorgx <kh.elallali@gmail.com>
2024-08-23 02:22:48 +0100

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,21 +7,22 @@ def validUTF8(data: List[int]) -> bool:
     """determines if a given data set represents a valid UTF-8 encoding"""
     continuation_bytes = 0
     for num in data:
-        if num < 0 or num > 255:
+        if not 0 <= num <= 255:
             return (False)
         if continuation_bytes == 0:
-            if (num >> 5) == 0b110:
+            if num >> 7 == 0b0:
+                continue
+            elif num >> 5 == 0b110:
                 continuation_bytes = 1
-            elif (num >> 4) == 0b1110:
+            elif num >> 4 == 0b1110:
                 continuation_bytes = 2
-            elif (num >> 3) == 0b11110:
+            elif num >> 3 == 0b11110:
                 continuation_bytes = 3
-            elif (num >> 7) != 0:
+            else:
                 return (False)
-
         else:
-            if (num >> 6) != 0b10:
+            if num >> 6 == 0b10:
+                continuation_bytes -= 1
+            else:
                 return (False)
-            remaining_bytes -= 1
-
     return (continuation_bytes == 0)
