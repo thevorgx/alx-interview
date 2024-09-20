2fa3b2533abd8f5052270b08dc9dc48d766e71d4
thevorgx <kh.elallali@gmail.com>
2024-09-20 03:56:09 +0100

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,10 +10,9 @@ def makeChange(coins, total):
     dp_arr = [float('inf')] * (total + 1)
     dp_arr[0] = 0
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp_arr[i] = min(dp_arr[i], dp_arr[i - coin] + 1)
-    if dp_arr[total] == float('inf'):
-        return -1
-    else:
-        return dp_arr[total]
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+                dp_arr[i] = min(dp_arr[i], dp_arr[i - coin] + 1)
+
+    return dp_arr[total] if dp_arr[total] != float('inf') else -1
